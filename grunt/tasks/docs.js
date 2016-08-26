5d96a2b0b473c8f4822a4f7d07d9284ce1dead8a
rerades <rerades@siete3.com>
2016-08-26 23:49:37 +0200

    added sassdoc examples and plugins

---
+++ grunt/tasks/docs.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rerades on 7/4/16.
+ */
+
+module.exports = function(grunt) {
+    // Default dev task without open.
+    grunt.registerTask('docs', [
+        'sassdoc'
+    ]);
+};
